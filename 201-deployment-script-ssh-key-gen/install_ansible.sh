8a719c68dec60f62db254ea7c2e6fa2d72153da4
Dongdong Yang <60205926@healthtech.nswhealth.net>
2020-06-10 20:50:54 +1000

    test parameters

--- 201-deployment-script-ssh-key-gen/install_ansible.sh
+++ 201-deployment-script-ssh-key-gen/install_ansible.sh
@@ -10,5 +10,11 @@ sudo apt install -y libssl-dev libffi-dev python-dev python-pip
 sudo pip install ansible[azure]
 
 ## Create a credentials file to store azure credentials
-## sudo mkdir ~/.azure
-## sudo touch ~/.azure/credentials
+sudo mkdir ~/.azure
+sudo touch ~/.azure/credentials
+
+echo "[default]" >> ~/.azure/credentials
+echo "subscription_id=a3a8e615-adf7-41d3-b249-8a58b5db8057" >> ~/.azure/credentials
+echo "client_id=a36f6653-ae19-4ddc-aad5-5296f6beebf4" >> ~/.azure/credentials
+echo "secret=af4f4eac-2023-4fb9-8de7-d880665ec360" >> ~/.azure/credentials
+echo "tenant=116a94d1-b8a6-4a14-9dd2-1aa9bfca17b4" >> ~/.azure/credentials
